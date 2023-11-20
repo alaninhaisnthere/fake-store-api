f1f616a1d739d1c3fb2ec8b1cbeefff098064f45
Alana Oliveira <alanaoliveira71@gmail.com>
2023-11-20 20:06:21 -0300

    chore: Updating navbar style and colapsible menu logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 import { FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
 
 const NavbarContainer = styled.div`
-  background-color: #0f52ba;
+  position: relative;
+  background-color: #FA997D;
   color: #fff;
   padding: 10px;
+  margin-bottom: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  position: fixed;
   width: 100%;
   z-index: 1000;
 `;
@@ -22,12 +23,14 @@ const Logo = styled.h1`
 const SearchContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 20px;
+  margin-right: auto;
+  margin-left: 20px;
+  flex: 1;
 `;
 
 const SearchInput = styled.input`
+  flex: 1;
   padding: 10px;
-  margin-right: 10px;
   border: none;
   border-radius: 4px;
 `;
@@ -38,7 +41,7 @@ const CartButton = styled.button`
   border: none;
   cursor: pointer;
   font-size: 24px;
-  margin-right: 20px;
+  margin-left: 20px;
 `;
 
 const CategoriesToggle = styled.button`
@@ -51,14 +54,18 @@ const CategoriesToggle = styled.button`
 `;
 
 const CategoriesContainer = styled.div<{ showCategories: boolean }>`
+  position: absolute;
+  top: 100%;
   display: ${(props) => (props.showCategories ? 'flex' : 'none')};
   flex-direction: column;
   align-items: flex-end;
   margin-top: 10px;
+  right: 40px;
+  width: max-content;
 `;
 
 const CategoryLink = styled.a`
-  color: #fff;
+  color: #000000;
   text-decoration: none;
   margin-top: 5px;
   font-size: 16px;
@@ -73,33 +80,33 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ categories }) => {
-  const [showCategories, setShowCategories] = useState(false);
-
-  const toggleCategories = () => {
-    setShowCategories(!showCategories);
+    const [showCategories, setShowCategories] = useState(false);
+  
+    const toggleCategories = () => {
+      setShowCategories(!showCategories);
+    };
+  
+    return (
+      <NavbarContainer>
+        <Logo>FakeStore</Logo>
+        <SearchContainer>
+          <SearchInput type="text" placeholder="Search products..." />
+          <CartButton>
+            <FaShoppingCart />
+          </CartButton>
+        </SearchContainer>
+        <CategoriesToggle onClick={toggleCategories}>
+          {showCategories ? <FaTimes /> : <FaBars />}
+        </CategoriesToggle>
+        <CategoriesContainer showCategories={showCategories}>
+          {categories.map((category) => (
+            <CategoryLink key={category} href={`#${category}`}>
+              {category}
+            </CategoryLink>
+          ))}
+        </CategoriesContainer>
+      </NavbarContainer>
+    );
   };
-
-  return (
-    <NavbarContainer>
-      <Logo>FakeStore</Logo>
-      <SearchContainer>
-        <SearchInput type="text" placeholder="Search products..." />
-        <CartButton>
-          <FaShoppingCart />
-        </CartButton>
-      </SearchContainer>
-      <CategoriesToggle onClick={toggleCategories}>
-        {showCategories ? <FaTimes /> : <FaBars />}
-      </CategoriesToggle>
-      <CategoriesContainer showCategories={showCategories}>
-        {categories.map((category) => (
-          <CategoryLink key={category} href={`#${category}`}>
-            {category}
-          </CategoryLink>
-        ))}
-      </CategoriesContainer>
-    </NavbarContainer>
-  );
-};
-
-export default Navbar;
+  
+  export default Navbar;
