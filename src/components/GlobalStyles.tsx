3916af5126309ae9106d0e55aa6f0a9cc017fa2d
Alana Oliveira <alanaoliveira71@gmail.com>
2023-11-26 23:27:10 -0300

    style: Styling login page and updating assets

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,12 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
   body {
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Poppins', sans-serif;
     margin: 0;
     padding: 0;
   }
-  
 `;
 
 export default GlobalStyles;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,53 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Logo from "../assets/logo.svg"
+import Image from "next/image";
 
 const LoginContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  height: 100vh;
+`;
+
+const LeftSection = styled.div`
+  flex: 0.6;
+  background-color: #fff;
+  display: flex;
   align-items: center;
-  margin-top: 50px;
+  justify-content: center;
+`;
+
+const RightSection = styled.div`
+  flex: 0.4;
+  background-color: #fbd02d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 300px;
+  width: 60%;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h1`
+  font-family: 'inter', sans-serif;
+  font-weight: bold;
+  font-size: 56px;
+  margin-bottom: 20px;
+  color: #1F3E82;
+`;
+
+const Subtitle = styled.p`
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.5);
+  margin-bottom: 20px;
 `;
 
 const InputField = styled.input`
@@ -24,7 +59,7 @@ const InputField = styled.input`
 
 const SubmitButton = styled.button`
   padding: 10px;
-  background-color: #fa997d;
+  background-color: #1F3E82;
   color: #fff;
   border: none;
   border-radius: 4px;
@@ -46,21 +81,28 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   return (
     <LoginContainer>
-      <LoginForm onSubmit={handleSubmit}>
-        <InputField
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <InputField
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <SubmitButton type="submit">Login</SubmitButton>
-      </LoginForm>
+      <LeftSection>
+        <LoginForm onSubmit={handleSubmit}>
+          <Title>Sign in</Title>
+          <Subtitle>Welcome back!</Subtitle>
+          <InputField
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputField
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <SubmitButton type="submit">Login</SubmitButton>
+        </LoginForm>
+      </LeftSection>
+      <RightSection>
+        <Image src={Logo} alt="Logo" />
+      </RightSection>
     </LoginContainer>
   );
 };
